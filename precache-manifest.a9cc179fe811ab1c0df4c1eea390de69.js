22bf390e74f9905987b24f29b0c19c2e81a929c3
Surajan Shrestha <surajanshrestha35@gmail.com>
2020-09-13 12:24:08 +0545

    Updates

---
+++ precache-manifest.a9cc179fe811ab1c0df4c1eea390de69.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5abd71d12a77e3c11952951669f27772",
+    "url": "/my-own-shopping-cart/index.html"
+  },
+  {
+    "revision": "2ff9a187fcf06dcb72ac",
+    "url": "/my-own-shopping-cart/static/css/main.82e790db.chunk.css"
+  },
+  {
+    "revision": "c0219efa1202cdb5165e",
+    "url": "/my-own-shopping-cart/static/js/2.e1a8a227.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-own-shopping-cart/static/js/2.e1a8a227.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2ff9a187fcf06dcb72ac",
+    "url": "/my-own-shopping-cart/static/js/main.3ace0314.chunk.js"
+  },
+  {
+    "revision": "2ebc08dd6b02567a675b",
+    "url": "/my-own-shopping-cart/static/js/runtime-main.fe54eb00.js"
+  }
+]);
